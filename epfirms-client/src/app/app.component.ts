1d1e125f33d4b3d862a0b1294f849880dc772297
Jaer Quijivix-Sweeney <jaer@epfirms.com>
2021-09-17 12:27:06 -0500

    Fix cases/leads/client directory overflow, Add initial loading screen

--- epfirms-client/src/app/app.component.ts
+++ epfirms-client/src/app/app.component.ts
@@ -1,25 +1,49 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { StripeCardElementOptions, StripeElementsOptions } from '@stripe/stripe-js';
-import { StripeCardComponent, StripeService } from 'ngx-stripe';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Component } from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationError,
+  ResolveEnd,
+  ResolveStart,
+  Router,
+  RouterEvent,
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  host: {
-    'class': ''
-  }
+  animations: [
+    trigger('fadeAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }), animate('300ms', style({ opacity: 1 }))]
+      ),
+      transition(':leave',
+        [style({ opacity: 1 }), animate('300ms', style({ opacity: 0 }))]
+      )
+    ])
+  ],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
+  loading: boolean = true;
 
-
-  constructor(private fb: FormBuilder, private stripeService: StripeService) {}
-
-  ngOnInit(): void {
+  constructor(private _router: Router) {
+    _router.events.subscribe((routerEvent: RouterEvent) => {
+      this.handleRouterEvent(routerEvent);
+    });
   }
 
-  createToken(): void {
+  handleRouterEvent(routerEvent: RouterEvent): void {
+    if (routerEvent instanceof ResolveStart) {
+      this.loading = true;
+    }
 
+    if (
+      routerEvent instanceof ResolveEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError
+    ) {
+      this.loading = false;
+    }
   }
 }
