5c94dc8c58fccf1cb63ca4ec75e44da447c1f02a
Jaer Quijivix-Sweeney <jaer@epfirms.com>
2021-10-11 12:18:17 -0500

    NGINX configuration for socket io

--- epfirms-client/src/environments/production/modules.prod.ts
+++ epfirms-client/src/environments/production/modules.prod.ts
@@ -1,6 +1,6 @@
 import { SocketIoConfig, SocketIoModule } from "ngx-socket-io";
 
-const config: SocketIoConfig = { url: 'http://localhost:4000', options: {autoConnect: false, transports: ['websocket']} };
+const config: SocketIoConfig = { url: 'https://app.epfirms.com', options: {autoConnect: false, transports: ['websocket']} };
 
 export const extModules = [
     SocketIoModule.forRoot(config),
