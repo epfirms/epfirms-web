89c598a91b02a72b0b99eda1343b7890af91c5d3
Bridger Jones <bridgerjones15@gmail.com>
2021-11-02 14:51:15 -0600

    adding bill manually now works

--- epfirms-client/src/app/firm-portal/matter-subtabs/billing/manage-bill/manage-bill.component.ts
+++ epfirms-client/src/app/firm-portal/matter-subtabs/billing/manage-bill/manage-bill.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatterService } from '@app/firm-portal/_services/matter-service/matter.service';
 
 @Component({
   selector: 'app-manage-bill',
@@ -11,13 +13,54 @@ export class ManageBillComponent implements OnInit {
   @Input() isVisible : boolean;
   @Output() isVisibleChange = new EventEmitter<boolean>();
 
-  constructor() { }
+  // matter properties
+  @Input() matter;
+
+  // bill form group
+  billForm = new FormGroup({
+    matter_id : new FormControl('', Validators.required),
+    amount: new FormControl(0),
+    // determines if bill or payment 0=bill 1=payment,
+    type: new FormControl(0),
+    date: new FormControl(new Date(), Validators.required),
+    hours: new FormControl(0, Validators.required),
+    description: new FormControl(''),
+    billing_type : new FormControl('', Validators.required),
+    payment_type: new FormControl('', Validators.required),
+    hourly_rate: new FormControl(null),
+    track_time_for: new FormControl(null,Validators.required),
+  })
+
+  //due date
+  date = new Date().toString()
+
+
+
+  constructor(private _matterService: MatterService) { }
 
   ngOnInit(): void {
+    console.log("Matter", this.matter);
+
+    //patch in the matter_id for the bill
+    this.billForm.patchValue({'matter_id': this.matter.id});
   }
 
   closeBillManager(): void {
     this.isVisibleChange.emit(false);
+  }
+
+  setDueDate(date): void {
+    this.billForm.patchValue({date: date});
+    console.log(this.billForm.value);
+  }
+
+  submit(): void {
+    let bill = this.billForm.value;
+    console.log(bill);
+    this._matterService.createBillOrPayment(bill).subscribe(res => {
+          console.log(res);
+          this.closeBillManager();
+         });
   }
 
 }
