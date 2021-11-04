5ac21e8309b1eee1bd20328f2cc54faad8d36a01
Bridger Jones <bridgerjones15@gmail.com>
2021-11-04 15:14:02 -0600

    adding basic crud to statement module

--- epfirms-api/src/modules/statement/controllers/statement.controller.ts
+++ epfirms-api/src/modules/statement/controllers/statement.controller.ts
@@ -1,10 +1,39 @@
 import { Response, Request } from 'express';
 import { StatusConstants } from '@src/constants/StatusConstants';
+import { StatementService } from '../services/statement.service';
 
 const passport = require('passport');
 
 export class StatementController {
   constructor() {}
+  public async create(req, res : Response) : Promise<any>{
+    try {
+      const created = await StatementService.create(req.body);
+      res.status(StatusConstants.CREATED).send(created);
+    } catch (err) {
+      console.error(err);
+      res.status(StatusConstants.INTERNAL_SERVER_ERROR).send(err);
+    }
+  }
+
+  public async delete(req, res : Response) : Promise<any>{
+    try {
+      const deleted = await StatementService.delete(req.params.id);
+      res.status(StatusConstants.OK).send({success: "true"});
+    } catch (err) {
+      console.error(err);
+      res.status(StatusConstants.INTERNAL_SERVER_ERROR).send(err);
+    }
+  }
+  public async update(req, res : Response) : Promise<any>{
+    try {
+      const updated = await StatementService.update(req.body);
+      res.status(StatusConstants.OK).send(updated);
+    } catch (err) {
+      console.error(err);
+      res.status(StatusConstants.INTERNAL_SERVER_ERROR).send(err);
+    }
+  }
 
 
 }
--- epfirms-api/src/modules/statement/services/statement.service.ts
+++ epfirms-api/src/modules/statement/services/statement.service.ts
@@ -2,5 +2,31 @@ import { Database } from '@src/core/Database';
 
 export class StatementService {
 
+  public static async create(data) : Promise<any> {
+    try {
+      const created = await Database.models.statement.create(data);
+      return Promise.resolve(created);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
+  public static async update(body) : Promise<any> {
+    try {
+      const updated = await Database.models.statement.update(body, {where: {id: body.id}});
+      return Promise.resolve(updated);
+    }
+    catch (err) {
+      console.error(err);
+    }
+  }
+  public static async delete(id) : Promise<any> {
+    try {
+      const deleted = await Database.models.statement.destroy({where: {id: id}});
+      return Promise.resolve(deleted);
+    }
+    catch (err) {
+      console.error(err);
+    }
+  }
 }
