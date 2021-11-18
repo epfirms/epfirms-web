a2489b7f28f590864c1464bfc87391d456510245
Bridger Jones <bridgerjones15@gmail.com>
2021-11-18 15:26:40 -0700

    starting work on .csv generation

--- epfirms-client/src/app/firm-portal/matter-subtabs/billing/billing.component.ts
+++ epfirms-client/src/app/firm-portal/matter-subtabs/billing/billing.component.ts
@@ -168,6 +168,19 @@ export class BillingComponent implements OnInit {
     this.currentStatement = statement;
   }
 
+  download(statement): void {
+    const rows = [
+    ["name1", "city1", "some other info"],
+    ["name2", "city2", "more info"]
+];
+
+let csvContent = "data:text/csv;charset=utf-8,"
+    + rows.map(e => e.join(",")).join("\n");
+
+    var encodedUri = encodeURI(csvContent);
+window.open(encodedUri);
+  }
+
 
 
 
