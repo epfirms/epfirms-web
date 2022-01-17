4d210fc6cc975dc016160585f8e3748a97117996
Bridger Jones <bridgerjones15@gmail.com>
2022-01-17 12:02:40 -0700

    addign legal-insurance module boilerplate

---
+++ epfirms-api/src/modules/legal-insurance/controllers/legal-insurance.controller.ts
@@ -0,0 +1,14 @@
+
+import { Response, Request } from 'express';
+import { StatusConstants } from '@src/constants/StatusConstants';
+import { LegalInsuranceService } from '@modules/legal-insurance/services/legal-insurance.service';
+import { Service } from 'typedi';
+
+@Service()
+export class LegalInsuranceController {
+  constructor() {}
+
+  
+}
+
+
---
+++ epfirms-api/src/modules/legal-insurance/routes/index.ts
@@ -0,0 +1,12 @@
+
+import express from 'express';
+import { legalInsuranceController} from '@modules/legal-insurance/controllers';
+
+const passport = require('passport');
+
+const legalInsuranceRouter = express.Router();
+
+export { legalInsuranceRouter };
+
+
+
