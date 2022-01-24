1137202c76dfc4ab034e59816f80560fb4e654f0
Bridger Jones <bridgerjones15@gmail.com>
2022-01-24 09:58:38 -0700

    load in all the bills that are associated with the statement

--- epfirms-client/src/app/client-portal/client-financials/statement-view/statement-view.component.ts
+++ epfirms-client/src/app/client-portal/client-financials/statement-view/statement-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatterService } from '@app/firm-portal/_services/matter-service/matter.service';
 
 @Component({
   selector: 'app-statement-view',
@@ -11,15 +12,32 @@ export class StatementViewComponent implements OnInit {
   @Input() isVisible : boolean;
   @Output() isVisibleChange = new EventEmitter<boolean>();
 
-  constructor() { }
+  //dictionary that will hold the bills based on statement id
+  // key => value will be matter_id => array of bills
+  statementBill = {};
+
+  constructor(
+    private matterService : MatterService
+  ) { }
 
   ngOnInit(): void {
     console.log(this.statements);
+    this.loadBills();
   }
 
   close() : void {
     this.isVisible = false;
     this.isVisibleChange.emit(this.isVisible);
+  }
+
+  loadBills() : void {
+    this.statements.forEach(statement => {
+      this.matterService.getMatterBillingById(statement.matter_id).subscribe(res => {
+        // get all of the reconciled bills associated with that statement_id
+        this.statementBill[statement.matter_id] = res.filter(bill => bill.type == 0 && bill.reconciled);
+        console.log(this.statementBill);
+      });
+    });
   }
 
 }
