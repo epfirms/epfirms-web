66cf9ac57a829b135f91f67e1b7f282ef8d4a5e2
Bridger Jones <bridgerjones15@gmail.com>
2022-01-28 14:02:52 -0700

    adding more to overview and some conditional stats

--- epfirms-client/src/app/features/billing/billing.module.ts
+++ epfirms-client/src/app/features/billing/billing.module.ts
@@ -6,17 +6,25 @@ import { EditableModule } from '@app/shared/editable/editable.module';
 import { LegalInsuranceComponent } from './legal-insurance/legal-insurance.component';
 import { MonthlyPaymentComponent } from './monthly-payment/monthly-payment.component';
 import { GenerateStatementComponent } from './generate-statement/generate-statement.component';
-
-
+import { OverviewComponent } from './overview/overview.component';
 
 @NgModule({
-  declarations: [ManageBillComponent, ManageStatementComponent, LegalInsuranceComponent, MonthlyPaymentComponent, GenerateStatementComponent],
-  imports: [
-    CoreModule,
-    EditableModule
+  declarations: [
+    ManageBillComponent,
+    ManageStatementComponent,
+    LegalInsuranceComponent,
+    MonthlyPaymentComponent,
+    GenerateStatementComponent,
+    OverviewComponent,
   ],
+  imports: [CoreModule, EditableModule],
   exports: [
-    ManageBillComponent, ManageStatementComponent, LegalInsuranceComponent, MonthlyPaymentComponent, GenerateStatementComponent
-  ]
+    ManageBillComponent,
+    ManageStatementComponent,
+    LegalInsuranceComponent,
+    MonthlyPaymentComponent,
+    GenerateStatementComponent,
+    OverviewComponent,
+  ],
 })
-export class BillingModule { }
+export class BillingModule {}
