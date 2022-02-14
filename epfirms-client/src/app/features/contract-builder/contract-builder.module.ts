a49b5c668b1e479d9b2d16220b7800f312271f02
Bridger Jones <bridgerjones15@gmail.com>
2022-02-14 14:29:39 -0700

    refactoring in a hard coded component for now to deal with the contracts

--- epfirms-client/src/app/features/contract-builder/contract-builder.module.ts
+++ epfirms-client/src/app/features/contract-builder/contract-builder.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContractEditorComponent } from './contract-editor/contract-editor.component';
+import { FlatRateComponent } from './flat-rate/flat-rate.component';
 
 
 
 @NgModule({
   declarations: [
-    ContractEditorComponent
+    ContractEditorComponent,
+    FlatRateComponent
   ],
   imports: [
     CommonModule
---
+++ epfirms-client/src/app/features/contract-builder/flat-rate/flat-rate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flat-rate',
+  templateUrl: './flat-rate.component.html',
+  styleUrls: ['./flat-rate.component.scss']
+})
+export class FlatRateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
