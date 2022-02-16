b6d35b4f39e10ef570643dfabdb0bf979c22b9ce
Bridger Jones <bridgerjones15@gmail.com>
2022-02-16 09:25:30 -0700

    removing the components that have been refactored into contract-editor

--- epfirms-client/src/app/features/contract-builder/contract-builder.module.ts
+++ epfirms-client/src/app/features/contract-builder/contract-builder.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContractEditorComponent } from './contract-editor/contract-editor.component';
-import { FlatRateComponent } from './flat-rate/flat-rate.component';
 import { EditableModule } from '@app/shared/editable/editable.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HourlyComponent } from './hourly/hourly.component';
-import { ContingencyComponent } from './contingency/contingency.component';
+
 
 
 
 @NgModule({
   declarations: [
     ContractEditorComponent,
-    FlatRateComponent,
-    HourlyComponent,
-    ContingencyComponent
+    
   ],
   imports: [
     CommonModule,
@@ -24,9 +20,7 @@ import { ContingencyComponent } from './contingency/contingency.component';
   ],
   exports: [
     ContractEditorComponent,
-    FlatRateComponent,
-    ContingencyComponent,
-    HourlyComponent
+    
   ]
 })
 export class ContractBuilderModule { }
