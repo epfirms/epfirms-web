6d6a61366ad5ee2ec5cdc146ec9b164e58bd31e2
Bridger Jones <bridgerjones15@gmail.com>
2022-02-17 12:03:38 -0700

    loading the matters and their associated contracts if they exist into the client-contracts; this will be given to child component contract-viewer to actually see the contract and then sign it

--- epfirms-client/src/app/client-portal/client-contracts/client-contracts.component.ts
+++ epfirms-client/src/app/client-portal/client-contracts/client-contracts.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Matter } from '@app/core/interfaces/matter';
+import { ContractService } from '@app/features/contract-builder/contract.service';
+import { MatterService } from '@app/firm-portal/_services/matter-service/matter.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-client-contracts',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientContractsComponent implements OnInit {
 
-  constructor() { }
+
+  contracts$ = Observable;
+  matters$ : Observable<Matter[]>;
+   // properties
+   matterList;
+   contractList = [];
+
+  constructor(
+    private contractService : ContractService,
+    private matterService : MatterService
+  ) {
+    // get the matters associated with the client
+    this.matters$ = this.matterService.entities$;
+   }
 
   ngOnInit(): void {
+    // get the matters from obs and convert to list
+    this.matters$.subscribe(res => {
+      this.matterList = res;
+      this.matterList.forEach(matter => {
+        this.loadContracts(matter.id);
+      });
+    });
+
+  }
+
+  loadContracts(matterId): void {
+    this.contractService.getWithMatterId(matterId).subscribe(res => this.contractList.push(res));
   }
 
 }
