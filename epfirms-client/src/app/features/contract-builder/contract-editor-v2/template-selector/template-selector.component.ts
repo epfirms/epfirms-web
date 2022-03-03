56b35a8048df4c40ad87f09dab534e44e1c9e940
bridger <bridgerjones15@gmail.com>
2022-03-03 09:29:54 -0600

    template selector now functional in the editor

--- epfirms-client/src/app/features/contract-builder/contract-editor-v2/template-selector/template-selector.component.ts
+++ epfirms-client/src/app/features/contract-builder/contract-editor-v2/template-selector/template-selector.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ContractService } from '../../contract.service';
 
 @Component({
   selector: 'app-template-selector',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-selector.component.scss']
 })
 export class TemplateSelectorComponent implements OnInit {
+  
+  @Output() selectedTemplate = new EventEmitter<object>();
 
-  constructor() { }
+  contractTemplates = [];
+
+  constructor(
+    private contractService : ContractService
+  ) { }
 
   ngOnInit(): void {
+    this.loadContractTemplates();
+  }
+
+  loadContractTemplates() : void {
+    this.contractService.getTemplatesByFirmId().subscribe(res => this.contractTemplates = res);
+  }
+
+  selectTemplate(templateContent) : void {
+    this.selectedTemplate.emit(templateContent);
   }
 
 }
