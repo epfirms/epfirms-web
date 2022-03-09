c1d62966cdc4e1ff327de565e2459eaa821bb7ba
bridger <bridgerjones15@gmail.com>
2022-03-09 11:02:15 -0600

    importing the angular forms into the module; add state and stack history to facilitate back()

--- epfirms-client/src/app/features/intake-v2/client-intake/client-intake.component.ts
+++ epfirms-client/src/app/features/intake-v2/client-intake/client-intake.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-intake.component.scss']
 })
 export class ClientIntakeComponent implements OnInit {
-
+  //state that manages the views
+  state : number = 0;
+  // stack that manages the views and enables the back() functionality
+  history = []; 
+  //personal information
+  personalInformation = {
+    isMarried: false
+  }
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  setState(state : number) : void {
+    this.history.push(this.state); 
+    this.state = state;
+    
+  }
+
+  back() : void {
+    this.state = this.history.pop();
   }
 
 }
--- epfirms-client/src/app/features/intake-v2/intake-v2.module.ts
+++ epfirms-client/src/app/features/intake-v2/intake-v2.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientIntakeComponent } from './client-intake/client-intake.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { ClientIntakeComponent } from './client-intake/client-intake.component';
     ClientIntakeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     ClientIntakeComponent
