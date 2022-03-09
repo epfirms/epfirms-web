4c835e16e9d64e7930e0cfd9f17984b7723b0821
bridger <bridgerjones15@gmail.com>
2022-03-09 13:28:50 -0600

    adding the rest of the fields

--- epfirms-client/src/app/features/intake-v2/client-intake/client-intake.component.ts
+++ epfirms-client/src/app/features/intake-v2/client-intake/client-intake.component.ts
@@ -3,35 +3,68 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-client-intake',
   templateUrl: './client-intake.component.html',
-  styleUrls: ['./client-intake.component.scss']
+  styleUrls: ['./client-intake.component.scss'],
 })
 export class ClientIntakeComponent implements OnInit {
   //state that manages the views
-  state : number = 0;
+  state: number = 0;
   // stack that manages the views and enables the back() functionality
-  history = []; 
+  history = [];
   //personal information
   personalInformation = {
+    first_name: '',
+    last_name: '',
+    full_name: '',
+    email: '',
+    address: '',
+    city: '',
+    zip: '',
+    dob: '',
+    phone: '',
+    state: '',
     isMarried: false,
-    dob: "",
-  }
+  };
   //spouse information
   spouseInformation = {
-    dob: "",
-  }
-  constructor() { }
+    first_name: '',
+    last_name: '',
+    full_name: '',
+    email: '',
+    address: '',
+    city: '',
+    zip: '',
+    dob: '',
+    phone: '',
+    state: '',
+  };
+  //children
+  children = [];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
-  setState(state : number) : void {
-    this.history.push(this.state); 
+  ngOnInit(): void {}
+
+  setState(state: number): void {
+    this.history.push(this.state);
     this.state = state;
-    
   }
 
-  back() : void {
+  back(): void {
     this.state = this.history.pop();
   }
 
+  addChild(): void {
+    this.children.push({
+      first_name: '',
+      last_name: '',
+      full_name: '',
+      email: '',
+      address: '',
+      city: '',
+      zip: '',
+      dob: '',
+      phone: '',
+      state: '',
+    });
+  }
 }
