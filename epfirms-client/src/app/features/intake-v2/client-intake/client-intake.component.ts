abb250f196b0ae614a9ae38fb14bcbdd5ca4900a
Bridger Jones <bridgerjones15@gmail.com>
2022-03-10 09:39:14 -0600

    writing the method that grabs the current user and prefills the intake form

--- epfirms-client/src/app/features/intake-v2/client-intake/client-intake.component.ts
+++ epfirms-client/src/app/features/intake-v2/client-intake/client-intake.component.ts
@@ -1,4 +1,6 @@
+import { W } from '@angular/cdk/keycodes';
 import { Component, OnInit } from '@angular/core';
+import { CurrentUserService } from '@app/shared/_services/current-user-service/current-user.service';
 
 @Component({
   selector: 'app-client-intake',
@@ -11,23 +13,25 @@ export class ClientIntakeComponent implements OnInit {
   // stack that manages the views and enables the back() functionality
   history = [];
   // state for spouse div
-  hasSpouse : boolean = false;
+  hasSpouse: boolean = false;
   //state for displaying children fields
-  hasChildren : boolean = false;
-  numOfChildren : number = 0;
+  hasChildren: boolean = false;
+  numOfChildren: number = 0;
   //personal information
-  personalInformation = {
-    first_name: '',
-    last_name: '',
-    full_name: '',
-    email: '',
-    address: '',
-    city: '',
-    zip: '',
-    dob: '',
-    phone: '',
-    state: '',
-  };
+  // personalInformation = {
+  //   first_name: '',
+  //   last_name: '',
+  //   full_name: '',
+  //   email: '',
+  //   address: '',
+  //   city: '',
+  //   zip: '',
+  //   dob: '',
+  //   phone: '',
+  //   state: '',
+  // };
+  personalInformation;
+  
   //spouse information
   spouseInformation = {
     first_name: '',
@@ -43,14 +47,37 @@ export class ClientIntakeComponent implements OnInit {
   };
   //children
   children = [];
+  // current user
+  user;
 
-  constructor() {}
+  constructor(private currentUserService: CurrentUserService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadCurrentUser();
+  }
 
+  loadCurrentUser(): void {
+    this.currentUserService.getCurrentUser().subscribe((res) => {
+      this.user = res.user;
+      this.initPersonalInformation();
+    });
+  }
   setState(state: number): void {
     this.history.push(this.state);
     this.state = state;
+  }
+  // used to fill in the current user information
+  initPersonalInformation() : void {
+   this.personalInformation = {
+    first_name: this.user.first_name,
+    last_name: this.user.last_name,
+    email: this.user.email,
+    address: this.user.address,
+    city: this.user.city,
+    zip: this.user.zip,
+    dob: this.user.dob,
+    state: this.user.state
+  } 
   }
 
   back(): void {
@@ -59,19 +86,18 @@ export class ClientIntakeComponent implements OnInit {
 
   addChild(): void {
     for (let i = 0; i < this.numOfChildren; i++) {
-     this.children.push({
-      first_name: '',
-      last_name: '',
-      full_name: '',
-      email: '',
-      address: '',
-      city: '',
-      zip: '',
-      dob: '',
-      phone: '',
-      state: '',
-    }); 
+      this.children.push({
+        first_name: '',
+        last_name: '',
+        full_name: '',
+        email: '',
+        address: '',
+        city: '',
+        zip: '',
+        dob: '',
+        phone: '',
+        state: '',
+      });
     }
-    
   }
 }
