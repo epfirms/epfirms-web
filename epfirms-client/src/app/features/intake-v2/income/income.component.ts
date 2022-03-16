1d8a65ff65fdb4818243c5459f477a6414c0e8a8
bridger <bridgerjones15@gmail.com>
2022-03-16 11:01:07 -0500

    integrating the income component; still needs backend

--- epfirms-client/src/app/features/intake-v2/income/income.component.ts
+++ epfirms-client/src/app/features/intake-v2/income/income.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FamilyMemberService } from '@app/client-portal/_services/family-member-service/family-member.service';
 import { Income } from '@app/core/interfaces/income';
 
 @Component({
@@ -7,15 +8,35 @@ import { Income } from '@app/core/interfaces/income';
   styleUrls: ['./income.component.scss']
 })
 export class IncomeComponent implements OnInit {
+  @Input() matter;
+  @Output() back = new EventEmitter<boolean>();
+  @Output() continue = new EventEmitter<boolean>();
+
   // states for the financials section
   includeSpouseIncome : boolean = false;
 
   // properties for the  monthly income section
   monthlyIncome : Income[] = [];
-  constructor() { }
+
+  // spouse property if available
+  spouse;
+
+
+  constructor(
+    private familyMemberService : FamilyMemberService,
+  ) { }
 
   ngOnInit(): void {
+  this.loadSpouse();
   }
+
+ loadSpouse() : void {
+    this.familyMemberService.getByUserId(this.matter.client.id).subscribe(res => {
+      console.log(res);
+      this.spouse = res.filter((member) => member.family_member.relationship_type === "spouse")[0];
+      console.log("spouse", this.spouse);
+    });
+ } 
   
   addIncome(isSpouseIncome : boolean) : void {
     console.log("ADDING INCOME FOR SPOUSE", isSpouseIncome);
@@ -30,6 +51,12 @@ export class IncomeComponent implements OnInit {
     incomeBinding.amount = parseFloat(income);
   }
 
+  backButton() : void {
+    this.back.emit(true);
+  }
 
+  continueButton() : void {
+    this.continue.emit(true);
+  }
 
 }
