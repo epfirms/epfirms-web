e18f48099b159cebe4455b7dd0f46cc5301f6d2e
bridger <bridgerjones15@gmail.com>
2022-03-16 09:52:54 -0500

    adding back and continue output bindings to facilitate when the state needs to move forward in the flow

--- epfirms-client/src/app/features/intake-v2/intake-dialog/intake-dialog.component.ts
+++ epfirms-client/src/app/features/intake-v2/intake-dialog/intake-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-intake-dialog',
@@ -8,9 +8,21 @@ import { Component, Input, OnInit } from '@angular/core';
 export class IntakeDialogComponent implements OnInit {
   // settings {title: string, content: string, backButton: boolean, continueButton: boolean}
   @Input() settings;
+
+  //output bindings to facilitate progress in the flow
+  @Output() back = new EventEmitter<boolean>();
+  @Output() continue = new EventEmitter<boolean>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  backButton() : void {
+    this.back.emit(true);
+  }
+
+  continueButton() : void {
+    this.continue.emit(true);
+  }
 }
