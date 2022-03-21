fc4a95d3aef823b917400757f2b7650251f2cdc9
bridger <bridgerjones15@gmail.com>
2022-03-21 09:58:46 -0500

    working on the assets, the form works just need to plug into backend

--- epfirms-client/src/app/features/intake-v2/assets/assets.component.ts
+++ epfirms-client/src/app/features/intake-v2/assets/assets.component.ts
@@ -1,39 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FamilyMemberService } from '@app/client-portal/_services/family-member-service/family-member.service';
 import { Asset } from '@app/core/interfaces/asset';
 
 @Component({
   selector: 'app-assets',
   templateUrl: './assets.component.html',
-  styleUrls: ['./assets.component.scss']
+  styleUrls: ['./assets.component.scss'],
 })
 export class AssetsComponent implements OnInit {
-// properties for the unprotected asset section
-  assets : Asset[] = [];
-  
+  // Input Bindings
+  @Input() matter;
+  // properties for the unprotected asset section
+  assets: Asset[] = [];
+
   // list of owners on potential assets
   owners = [];
-  constructor() { }
+
+  // spouse of client
+  spouse;
+  constructor(private familyMemberService: FamilyMemberService) {}
 
   ngOnInit(): void {
+    this.owners.push(this.matter.client);
+    this.loadSpouse();
+
+
   }
-addAsset(isProtected : boolean) : void {
+  addAsset(isProtected: boolean): void {
     this.assets.push({
-      name: "Enter Name",
-    amount : 0,
-    type : "Checking",
-    is_protected : isProtected,
-    owners : {}
+      name: 'Enter Name',
+      amount: 0,
+      type: 'Checking',
+      is_protected: isProtected,
+      owners: {},
     });
   }
 
-  handleOwnerSelection(event, asset, owner) : void {
-     if (event.target.checked) {
-      asset.owners[owner.id] = owner.first_name;
+  handleOwnerSelection(event, asset, owner): void {
+    if (event.target.checked) {
+      asset.owners[owner.id] = true;
+    } else if (!event.target.checked) {
+      asset.owners[owner.id] = false;
     }
-    else if (!event.target.checked){
-      asset.owners[owner.id] = null;
-    }
-       console.log(asset);
-
+    console.log(asset);
+  }
+  loadSpouse(): void {
+    this.familyMemberService.getByUserId(this.matter.client.id).subscribe((res) => {
+      console.log(res);
+      this.spouse = res.filter((member) => member.family_member.relationship_type === 'spouse')[0];
+      this.owners.push(this.spouse);
+    });
   }
 }
