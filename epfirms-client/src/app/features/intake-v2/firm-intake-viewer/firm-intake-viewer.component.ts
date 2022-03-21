46afb9056697472d83f2c951802776f14f9e7da5
bridger <bridgerjones15@gmail.com>
2022-03-21 17:27:36 -0500

    adding most of logic for the different types of children; needs to have all of the methods modified for the different types

--- epfirms-client/src/app/features/intake-v2/firm-intake-viewer/firm-intake-viewer.component.ts
+++ epfirms-client/src/app/features/intake-v2/firm-intake-viewer/firm-intake-viewer.component.ts
@@ -6,17 +6,20 @@ import { ClientService } from '@app/firm-portal/_services/client-service/client.
 @Component({
   selector: 'app-firm-intake-viewer',
   templateUrl: './firm-intake-viewer.component.html',
-  styleUrls: ['./firm-intake-viewer.component.scss']
+  styleUrls: ['./firm-intake-viewer.component.scss'],
 })
 export class FirmIntakeViewerComponent implements OnInit {
-
   @Input() matter;
   // state for spouse div
   hasSpouse: boolean = false;
   //state for displaying children fields
   hasChildren: boolean = false;
-  numOfChildren: number = 0;
+  hasGrandChildren : boolean = false;
+  numOfMinorChildren: number = 0;
+  numOfAdultChildren: number = 0;
+  numOfGrandChildren: number = 0;
   
+
   client = {
     id: undefined,
     first_name: '',
@@ -27,109 +30,110 @@ export class FirmIntakeViewerComponent implements OnInit {
     city: '',
     zip: '',
     dob: undefined,
-    relationship_type : "spouse",
+    relationship_type: 'spouse',
     phone: '',
     state: '',
     county: '',
     ssn: '',
     drivers_id: '',
-    note: ''
+    note: '',
   };
- 
+
   //spouse information
   spouse;
- 
+
   //children
-  children;
+  minorChildren;
+  adultChildren;
+  grandChildren;
 
   constructor(
-    private clientService : ClientService,
-    private familyMemberService : FamilyMemberService,
-    private clientMatterService : ClientMatterService,
-  ) { }
+    private clientService: ClientService,
+    private familyMemberService: FamilyMemberService,
+    private clientMatterService: ClientMatterService,
+  ) {}
 
   ngOnInit(): void {
     this.loadClientData();
     this.loadExistingFamilyData();
-    console.log("MATTER", this.matter);
+    console.log('MATTER', this.matter);
   }
 
-  
-
-
-  loadExistingFamilyData() : void {
-    this.familyMemberService.getByUserId(this.matter.client.id).subscribe(res => {
-      this.spouse = res.filter((member) => member.family_member.relationship_type === "spouse")[0];
-      this.children = res.filter((member) => member.family_member.relationship_type === 'child');
+  loadExistingFamilyData(): void {
+    this.familyMemberService.getByUserId(this.matter.client.id).subscribe((res) => {
+      this.spouse = res.filter((member) => member.family_member.relationship_type === 'spouse')[0];
+      // get all minor children
+      this.minorChildren = res.filter(
+        (member) => member.family_member.relationship_type === 'child' && member.is_minor
+      );
+      // get all adult children
+      this.adultChildren = res.filter(
+        (member) => member.family_member.relationship_type === 'child' && !member.is_minor
+      );
+      // get all grandchildren
+       this.grandChildren = res.filter(
+        (member) => member.family_member.relationship_type === 'grandchild' 
+      );
+ 
       if (this.spouse) {
         this.hasSpouse = true;
       }
-      if (this.children.length !== 0) {
+      if (this.minorChildren.length !== 0 || this.adultChildren.length !== 0) {
         this.hasChildren = true;
-        this.numOfChildren = this.children.length;
+        this.numOfMinorChildren = this.minorChildren.length;
+        this.numOfAdultChildren = this.adultChildren.length;
+      }
+      if (this.grandChildren.length !== 0) {
+        this.hasGrandChildren = true;
+        this.numOfGrandChildren = this.grandChildren.length;
       }
     });
   }
 
-  loadClientData() : void {
+  loadClientData(): void {
     let client = this.matter.client;
-     this.client.id = client.id;
-     this.client.first_name = client.first_name;
-     this.client.last_name = client.last_name;
-     this.client.full_name = client.full_name;
-     this.client.email = client.email;
-     this.client.phone = client.phone;
-     this.client.address = client.address;
-     this.client.city = client.city;
-     this.client.state = client.state;
-     this.client.zip = client.zip;
-     this.client.county = client.county;
-     this.client.ssn = client.ssn;
-     this.client.drivers_id = client.drivers_id;
-     this.client.note = client.note; 
+    this.client.id = client.id;
+    this.client.first_name = client.first_name;
+    this.client.last_name = client.last_name;
+    this.client.full_name = client.full_name;
+    this.client.email = client.email;
+    this.client.phone = client.phone;
+    this.client.address = client.address;
+    this.client.city = client.city;
+    this.client.state = client.state;
+    this.client.zip = client.zip;
+    this.client.county = client.county;
+    this.client.ssn = client.ssn;
+    this.client.drivers_id = client.drivers_id;
+    this.client.note = client.note;
   }
 
+  // addChild(isMinor : boolean, isGrandChild : boolean): void {
+  //   // implement add child for the different types
+  //   console.log(isMinor);
+  // }
 
-  addChild(): void {
-    this.children = [];
-    for (let i = 0; i < this.numOfChildren; i++) {
-      this.children.push({
-        first_name: '',
-        last_name: '',
-        full_name: '',
-        email: '',
-        address: '',
-        relationship_type: "child",
-        city: '',
-        zip: '',
-        dob: new Date().toString(),
-        phone: '',
-        state: '',
-      });
-    }
-  }
+  // addChildren(num): void {
+  //   this.numOfChildren = parseInt(num);
+  //   this.addChild();
+  // }
 
-  addChildren(num) : void {
-    this.numOfChildren = parseInt(num);
-    this.addChild();
-  }
-
-  submitClient() : void {
+  submitClient(): void {
     this.clientService.updateClient(this.client).subscribe();
   }
 
-  submitSpouse() : void {
-    this.spouse.relationship_type = "spouse";
-    this.familyMemberService.addFamilyMemberForUser(this.client.id, this.spouse).subscribe((res) => {
-     
-    });   
+  submitSpouse(): void {
+    this.spouse.relationship_type = 'spouse';
+    this.familyMemberService
+      .addFamilyMemberForUser(this.client.id, this.spouse)
+      .subscribe((res) => {});
   }
 
-  submitChildren() : void {
-    this.children.forEach(child => {
-      this.familyMemberService.addFamilyMemberForUser(this.client.id, child).subscribe();
-    });
-  }
+  // submitChildren(): void {
+  //   this.children.forEach((child) => {
+  //     this.familyMemberService.addFamilyMemberForUser(this.client.id, child).subscribe();
+  //   });
+  // }
 
   submitPersonalInformation(): void {
     this.submitClient();
@@ -137,10 +141,7 @@ export class FirmIntakeViewerComponent implements OnInit {
       this.submitSpouse();
     }
     if (this.hasChildren) {
-      this.submitChildren();
+      // this.submitChildren();
     }
-    
-   
   }
-
 }
