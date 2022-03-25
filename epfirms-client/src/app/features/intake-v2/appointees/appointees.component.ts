e519dceeb1db1a4d2ca087f38d43b485cd25f2fd
Bridger Jones <bridgerjones15@gmail.com>
2022-03-25 13:34:57 -0500

    adding method to load in family members

--- epfirms-client/src/app/features/intake-v2/appointees/appointees.component.ts
+++ epfirms-client/src/app/features/intake-v2/appointees/appointees.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FamilyMemberService } from '@app/client-portal/_services/family-member-service/family-member.service';
 
 @Component({
   selector: 'app-appointees',
@@ -9,9 +10,27 @@ export class AppointeesComponent implements OnInit {
   @Input() matter;
   @Output() back = new EventEmitter<boolean>();
   @Output() continue = new EventEmitter<boolean>();
-  constructor() {}
+  
 
-  ngOnInit(): void {}
+  familyMembers = [];
+
+  constructor(
+    private familyMemberService : FamilyMemberService
+  ) {}
+
+  ngOnInit(): void {
+
+    this.loadFamilyMembers();
+  }
+
+  loadFamilyMembers() : void {
+    this.familyMemberService.getByUserId(this.matter.client.id).subscribe(res => {
+      console.log("FAMILY", res);
+      if (res.length != 0) {
+        this.familyMembers = res;
+      }
+    });
+  }
   backButton(): void {
     this.back.emit(true);
   }
