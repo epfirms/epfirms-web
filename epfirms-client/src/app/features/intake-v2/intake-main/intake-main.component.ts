d0d68b3eb3508f31f3620c6bce1ad7a080ab153b
bridger <bridger@epfirms.com>
2022-03-28 13:34:22 -0500

    adding the matter entities into the component

--- epfirms-client/src/app/features/intake-v2/intake-main/intake-main.component.ts
+++ epfirms-client/src/app/features/intake-v2/intake-main/intake-main.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ClientMatterService } from '@app/client-portal/_services/matter-service/client-matter.service';
+import { Matter } from '@app/core/interfaces/matter';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-intake-main',
   templateUrl: './intake-main.component.html',
   styleUrls: ['./intake-main.component.scss']
 })
-export class IntakeMainComponent implements OnInit {
+export class IntakeMainComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  matters$ : Observable<Matter[]>
+
+
+  constructor(
+    private clientMatterService : ClientMatterService
+  ) {
+
+      this.matters$ = this.clientMatterService.entities$;
+   }
 
   ngOnInit(): void {
   }
 
+
+
+  ngOnDestroy(): void {
+    
+  }
+
+  
 }
