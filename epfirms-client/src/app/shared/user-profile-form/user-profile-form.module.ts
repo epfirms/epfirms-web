a3cc4801279d3fbf4a9f791b995dd0e7b8b74860
bridger <bridger@epfirms.com>
2022-03-30 16:33:46 -0500

    adding forms module to user profile form component module; adding bindings to the template

--- epfirms-client/src/app/shared/user-profile-form/user-profile-form.module.ts
+++ epfirms-client/src/app/shared/user-profile-form/user-profile-form.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserProfileFormComponent } from './user-profile-form/user-profile-form.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { UserProfileFormComponent } from './user-profile-form/user-profile-form.
     UserProfileFormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
   ],
   exports: [
     UserProfileFormComponent,
