19e065318fc97fbf6fd600716536f7a4ab05b0db
bridger <bridger@epfirms.com>
2022-04-04 11:14:47 -0500

    adding merge of family members and appointess, this allows accurate loading of appointees

--- epfirms-client/src/app/features/intake-v2/appointees/appointees.component.ts
+++ epfirms-client/src/app/features/intake-v2/appointees/appointees.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AppointeeService } from '@app/client-portal/_services/appointee-service/appointee.service';
 import { FamilyMemberService } from '@app/client-portal/_services/family-member-service/family-member.service';
 
 @Component({
@@ -13,9 +14,11 @@ export class AppointeesComponent implements OnInit {
   
 
   familyMembers = [];
+  appointees = [];
 
   constructor(
-    private familyMemberService : FamilyMemberService
+    private familyMemberService : FamilyMemberService,
+    private appointeeService : AppointeeService
   ) {}
 
   ngOnInit(): void {
@@ -23,13 +26,39 @@ export class AppointeesComponent implements OnInit {
     this.loadFamilyMembers();
   }
 
-  loadFamilyMembers() : void {
+private  loadFamilyMembers() : void {
     this.familyMemberService.getByUserId(this.matter.client.id).subscribe(res => {
       console.log("FAMILY", res);
       if (res.length != 0) {
         this.familyMembers = res;
+        this.loadAppointees(this.familyMembers);
       }
     });
+  }
+
+  private loadAppointees(familyMembers) : void {
+    this.appointeeService.getByUserId(this.matter.client.id).subscribe(res => {
+      // if there are appointees, we need to map them to family members and then add to list of
+      // appointees
+      
+      if (res) {
+        console.log("family members", familyMembers);
+        console.log("appointees", res);
+        this.mergeIntoAppointees(familyMembers,res);
+      }
+
+    });
+  }
+
+  private mergeIntoAppointees(familyMembers, appointees) : void {
+    familyMembers.forEach(member => {
+      appointees.forEach(appointee => {
+        if (member.id === appointee.id){
+          this.appointees.push(member);
+        }
+      });
+    });
+    console.log("merged appintees and fam", this.appointees);
   }
   backButton(): void {
     this.back.emit(true);
