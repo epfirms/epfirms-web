97ae8780e99c0de03154b62c6c605c43ed5bbed0
bridger <bridger@epfirms.com>
2022-04-04 14:56:31 -0500

    adding spouse appointees logic

--- epfirms-client/src/app/features/intake-v2/appointees/appointees.component.ts
+++ epfirms-client/src/app/features/intake-v2/appointees/appointees.component.ts
@@ -11,56 +11,89 @@ export class AppointeesComponent implements OnInit {
   @Input() matter;
   @Output() back = new EventEmitter<boolean>();
   @Output() continue = new EventEmitter<boolean>();
-  
 
+  spouse;
+  client;
   familyMembers = [];
   appointees = [];
+  spouseAppointees = [];
 
   constructor(
-    private familyMemberService : FamilyMemberService,
-    private appointeeService : AppointeeService
+    private familyMemberService: FamilyMemberService,
+    private appointeeService: AppointeeService,
   ) {}
 
   ngOnInit(): void {
-
     this.loadFamilyMembers();
   }
 
-private  loadFamilyMembers() : void {
-    this.familyMemberService.getByUserId(this.matter.client.id).subscribe(res => {
-      console.log("FAMILY", res);
+  private loadFamilyMembers(): void {
+    this.familyMemberService.getByUserId(this.matter.client.id).subscribe((res) => {
+      console.log('FAMILY', res);
       if (res.length != 0) {
         this.familyMembers = res;
         this.loadAppointees(this.familyMembers);
+        this.loadSpouseAppointees();
+        this.checkForSpouse();
       }
     });
   }
 
-  private loadAppointees(familyMembers) : void {
-    this.appointeeService.getByUserId(this.matter.client.id).subscribe(res => {
+  private checkForSpouse(): void {
+    this.familyMembers.forEach((member) => {
+      if (member.relationship_type == 'spouse') {
+        this.spouse = member;
+      }
+    });
+  }
+
+  private loadAppointees(familyMembers): void {
+    this.appointeeService.getByUserId(this.matter.client.id).subscribe((res) => {
       // if there are appointees, we need to map them to family members and then add to list of
       // appointees
-      
-      if (res) {
-        console.log("family members", familyMembers);
-        console.log("appointees", res);
-        this.mergeIntoAppointees(familyMembers,res);
-      }
 
+      if (res) {
+        console.log('family members', familyMembers);
+        console.log('appointees', res);
+        this.mergeIntoAppointees(familyMembers, res);
+      }
     });
   }
 
-  private mergeIntoAppointees(familyMembers, appointees) : void {
-    familyMembers.forEach(member => {
-      appointees.forEach(appointee => {
-        if (member.id === appointee.id){
+  private loadSpouseAppointees(): void {
+    if (this.spouse) {
+      this.appointeeService.getByUserId(this.spouse.id).subscribe((res) => {
+        if (res) {
+          this.spouseAppointees = res;
+          this.mergeIntoSpouseAppointees(this.familyMembers, this.spouseAppointees);
+        }
+      });
+    }
+  }
+
+  private mergeIntoSpouseAppointees(familyMembers, appointees): void {
+    familyMembers.forEach((member) => {
+      appointees.forEach((appointee) => {
+        if (member.id === appointee.id) {
+          member.appointee = appointee.appointee;
+          this.spouseAppointees.push(member);
+        }
+      });
+    });
+    console.log('merged spouse appintees and fam', this.spouseAppointees);
+  }
+  private mergeIntoAppointees(familyMembers, appointees): void {
+    familyMembers.forEach((member) => {
+      appointees.forEach((appointee) => {
+        if (member.id === appointee.id) {
           member.appointee = appointee.appointee;
           this.appointees.push(member);
         }
       });
     });
-    console.log("merged appintees and fam", this.appointees);
+    console.log('merged appintees and fam', this.appointees);
   }
+
   backButton(): void {
     this.back.emit(true);
   }
