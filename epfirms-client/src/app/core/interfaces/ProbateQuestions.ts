eb5e583fe8094df4a6b91c5af130d6f7930c19ea
bridger <bridger@epfirms.com>
2022-04-18 15:28:25 -0500

    adding component for the probate and a class for it

---
+++ epfirms-client/src/app/core/interfaces/ProbateQuestions.ts
@@ -0,0 +1,14 @@
+export class ProbateQuestions {
+    place_of_birth: string;
+    date_of_death: string;
+    place_of_death: string;
+    us_citizen: boolean;
+    naturalized_citizen: boolean;
+    date_of_naturalization: string;
+    place_of_naturalization: string;
+    date_of_will: string;
+    date_of_codicils: string;
+    personal_representative_id: number;
+    alternative_representative_id: number;
+    
+}
---
+++ epfirms-client/src/app/features/intake-v2/probate-intake/probate-intake.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-probate-intake',
+  templateUrl: './probate-intake.component.html',
+  styleUrls: ['./probate-intake.component.scss']
+})
+export class ProbateIntakeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
