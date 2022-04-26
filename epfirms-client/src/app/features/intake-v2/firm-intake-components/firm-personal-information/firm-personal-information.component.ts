4d12057b338d2c65179bf36508c532ee77689285
Bridger Jones <bridgerjones15@gmail.com>
2022-04-26 16:52:15 -0500

    basic UI for new personal info component is done; adding bindings for client and spouse; loads client and spouse;

--- epfirms-client/src/app/features/intake-v2/firm-intake-components/firm-personal-information/firm-personal-information.component.ts
+++ epfirms-client/src/app/features/intake-v2/firm-intake-components/firm-personal-information/firm-personal-information.component.ts
@@ -19,9 +19,51 @@ export class FirmPersonalInformationComponent implements OnInit {
 
   // holds the properties of the client
   client;
+  
+
+  // spouse
+  spouse;
 
   //array that holds family members
   familyMembers = [];
+
+
+  // client form
+  clientForm = {
+    id: undefined,
+    first_name: '',
+    last_name: '',
+    email: '',
+    phone: '',
+    cell_phone: '',
+    address: '',
+    city: '',
+    state: '',
+    zip: '',
+    dob: '',
+    ssn: '',
+    drivers_id: '',
+  }
+
+
+  // client form
+  spouseForm = {
+    id: undefined,
+    
+    first_name: '',
+    last_name: '',
+    email: '',
+    phone: '',
+    cell_phone: '',
+    address: '',
+    city: '',
+    state: '',
+    zip: '',
+    dob: '',
+    ssn: '',
+    drivers_id: '',
+  }
+
 
 
   constructor(
@@ -40,33 +82,50 @@ export class FirmPersonalInformationComponent implements OnInit {
       .getByUserId(this.client.id)
       .subscribe((familyMembers) => {
         this.familyMembers = familyMembers;
+        console.log("family members", this.familyMembers);
+        this.loadSpouse();
       });
+  }
+  private loadSpouse() : void {
+    this.spouse = this.familyMembers.filter(member => member.family_member.relationship_type === 'spouse')[0];
+    if (this.spouse) {
+      this.spouseForm.id = this.spouse.id;
+      this.spouseForm.first_name = this.spouse.first_name;
+      this.spouseForm.last_name = this.spouse.last_name;
+      this.spouseForm.email = this.spouse.email;
+      this.spouseForm.phone = this.spouse.phone;
+      this.spouseForm.cell_phone = this.spouse.cell_phone;
+      this.spouseForm.address = this.spouse.address;
+      this.spouseForm.city = this.spouse.city;
+      this.spouseForm.state = this.spouse.state;
+      this.spouseForm.zip = this.spouse.zip;
+      this.spouseForm.dob = this.spouse.dob;
+      this.spouseForm.ssn = this.spouse.ssn;
+      this.spouseForm.drivers_id = this.spouse.drivers_id;
+    }
   }
 
   private loadClientData(): void {
     this.client = this.matter.client;
+    console.log("client", this.client);
+
+    this.clientForm.id = this.client.id;
+    this.clientForm.first_name = this.client.first_name;
+    this.clientForm.last_name = this.client.last_name;
+    this.clientForm.email = this.client.email;
+    this.clientForm.phone = this.client.phone;
+    this.clientForm.cell_phone = this.client.cell_phone;
+    this.clientForm.address = this.client.address;
+    this.clientForm.city = this.client.city;
+    this.clientForm.state = this.client.state;
+    this.clientForm.zip = this.client.zip;
+    this.clientForm.dob = this.client.dob;
+    this.clientForm.ssn = this.client.ssn;
+    this.clientForm.drivers_id = this.client.drivers_id;
+
   }
 
-  addFamilyMember(): void {
-    let member = {
-      first_name: '',
-      last_name: '',
-      family_member: {relationship_type: ''},
-      email: '',
-      phone: '',
-      address: '',
-      city: '',
-      state: '',
-      zip: '',
-      county: '',
-      ssn: '',
-      dob: '',
-      template : true,
-      drivers_id: '',
-
-    }
-    this.familyMembers.push(member);
-  }
+  
   backButton(): void {
     this.back.emit(true);
   }
