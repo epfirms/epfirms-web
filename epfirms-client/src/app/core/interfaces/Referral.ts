a5e3b1db63b6eb23ce7fef0f132d563b417e03a4
bridger <bridger@epfirms.com>
2022-05-04 11:29:22 -0500

    adding new class for Referral on frontend; should facicilate easier creation of referral objects

---
+++ epfirms-client/src/app/core/interfaces/Referral.ts
@@ -0,0 +1,23 @@
+export class Referral {
+    id?: number;
+    matter_id: number;
+    date: string;
+    friend_or_family: boolean;
+    professional: boolean;
+    referral_name?: string;
+    google: boolean;
+    bing: boolean;
+    other_search_engine: boolean;
+    facebook: boolean;
+    twitter: boolean;
+    other_social_media: boolean;
+    aarag: boolean;
+    metlegal: boolean;
+    other_legal_insurance: boolean;
+    other: boolean;
+
+    constructor(matter_id: number) {
+        this.matter_id = matter_id;
+        this.date = new Date().toISOString();
+    }
+}
