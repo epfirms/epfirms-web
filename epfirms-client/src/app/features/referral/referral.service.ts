22044700d2bce662acd4c393fbccfc00f6ffd173
bridger <bridger@epfirms.com>
2022-05-04 15:10:47 -0500

    adding frontend service for referral

--- epfirms-client/src/app/features/referral/referral.service.ts
+++ epfirms-client/src/app/features/referral/referral.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReferralService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  upsert(data) : Observable<any> {
+    return this.http.post('/api/referral', data);
+  }
+
+  getOneWithMatterId(matter_id) : Observable<any> {
+    return this.http.get('/api/referral/' + matter_id);
+  }
+
+  getAllWithFirmId(firm_id) : Observable<any> {
+    return this.http.get('/api/referral/firm/' + firm_id);
+  }
+
+  delete(id) : Observable<any> {
+    return this.http.delete('/api/referral/' + id);
+  }
+
+
 }
