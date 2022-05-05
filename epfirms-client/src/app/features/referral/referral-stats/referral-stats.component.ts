0192bd05291047ae2702fb6cb57cad2a2a015c67
Bridger Jones <bridgerjones15@gmail.com>
2022-05-05 14:42:56 -0500

    integrating vanilla Chart.js into referrals

--- epfirms-client/src/app/features/referral/referral-stats/referral-stats.component.ts
+++ epfirms-client/src/app/features/referral/referral-stats/referral-stats.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Chart, ChartType} from 'chart.js';
+import { ChartConfiguration, LineController, LineElement, PointElement, LinearScale, Title, CategoryScale} from 'chart.js'
+
 
 @Component({
   selector: 'app-referral-stats',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReferralStatsComponent implements OnInit {
 
-  constructor() { }
+  linechartType : ChartType = 'line';
+  labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+
+  data = {
+    labels: this.labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
+
+  config = {
+    type: this.linechartType,
+    data: this.data,
+    options: {}
+  };
+  constructor() {
+    Chart.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale);
+   }
 
   ngOnInit(): void {
+    const ctx = document.getElementById('chart') as HTMLCanvasElement;
+    const myChart = new Chart(
+     ctx, 
+      this.config
+    );
   }
 
 }
