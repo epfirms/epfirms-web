cfb127f66801d71d1cef2f31ece09e5ebcefc315
bridger <bridger@epfirms.com>
2022-05-09 15:02:01 -0500

    building out the probate workflow; fixing the null error in the firm intake viewer;

--- epfirms-client/src/app/features/intake-v2/probate-workflow/probate-workflow.component.ts
+++ epfirms-client/src/app/features/intake-v2/probate-workflow/probate-workflow.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Matter } from '@app/core/interfaces/matter';
 
 @Component({
   selector: 'app-probate-workflow',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProbateWorkflowComponent implements OnInit {
 
+  //input bindings
+  @Input() matter : Matter;
+
+  // state manages the view
+  state : string = 'personal representative';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  setState(state : string) {
+    this.state = state;
+  }
+
 
 }
