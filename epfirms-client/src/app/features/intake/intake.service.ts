b5bea7d4c580fd74eaf3167f8202df03b7b18ec2
bridger <bridger@epfirms.com>
2022-05-09 14:00:24 -0500

    adding service to manage just intakes

--- epfirms-client/src/app/features/intake/intake.service.ts
+++ epfirms-client/src/app/features/intake/intake.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IntakeService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  getOneWithMatterId(id) : Observable<any> {
+    return this.http.get(`/api/intake/${id}`);
+  }
+
+  upsert(data) : Observable<any> {
+    return this.http.post(`/api/intake`, data);
+  }
+
+  delete(id) : Observable<any> {
+    return this.http.delete(`/api/intake/${id}`);
+  }
+
+  
 }
