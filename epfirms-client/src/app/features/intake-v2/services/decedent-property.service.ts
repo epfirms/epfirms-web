16b8966595e6cff3be0b6cb3f2c475d5d3a3ce36
bridger <bridger@epfirms.com>
2022-05-11 13:24:29 -0500

    adding methods to the decedent property service that I forgot to do"

--- epfirms-client/src/app/features/intake-v2/services/decedent-property.service.ts
+++ epfirms-client/src/app/features/intake-v2/services/decedent-property.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DecedentPropertyService {
 
-  constructor() { }
+  constructor(private http : HttpClient) {
+
+   }
+
+
+   getDecedentPropertyWithDecedentId(decedentId : number) : Observable<any> {
+      return this.http.get(`/api/decedent-property/${decedentId}`);
+   }
+
+   upsert(decedentProperty : any) : Observable<any> {
+      return this.http.post(`/api/decedent-property`, decedentProperty);
+   }
+   delete(decedentProperty : any) : Observable<any> {
+      return this.http.delete(`/api/decedent-property/${decedentProperty.id}`);
+   }
+
 }
