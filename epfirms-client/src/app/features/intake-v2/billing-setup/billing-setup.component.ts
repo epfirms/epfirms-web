e58f8c722a0a0324cc73c707ed3af04dafc2a089
bridger <bridger@epfirms.com>
2022-05-18 13:45:03 -0500

    adding billing setup component for the intake

---
+++ epfirms-client/src/app/features/intake-v2/billing-setup/billing-setup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-billing-setup',
+  templateUrl: './billing-setup.component.html',
+  styleUrls: ['./billing-setup.component.scss']
+})
+export class BillingSetupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
