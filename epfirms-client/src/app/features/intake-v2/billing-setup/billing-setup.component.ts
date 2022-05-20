f0e7cda99655b749ee837aa5fcffb9cc92fe6e3d
Bridger Jones <bridgerjones15@gmail.com>
2022-05-20 10:50:08 -0500

    billing setup in the intake essentially works

--- epfirms-client/src/app/features/intake-v2/billing-setup/billing-setup.component.ts
+++ epfirms-client/src/app/features/intake-v2/billing-setup/billing-setup.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Matter } from '@app/core/interfaces/matter';
+import { MatterBillingSettings } from '@app/core/interfaces/MatterBillingSettings';
+import { MatterBillingSettingsService } from '@app/shared/_services/matter-billing-settings-service/matter-billing-settings.service';
 import { createMask } from '@ngneat/input-mask';
 
 @Component({
   selector: 'app-billing-setup',
   templateUrl: './billing-setup.component.html',
-  styleUrls: ['./billing-setup.component.scss']
+  styleUrls: ['./billing-setup.component.scss'],
 })
 export class BillingSetupComponent implements OnInit {
-
-  billingType : string;
-  splitPayment : boolean = false;
+  @Input() matter: Matter;
+  billingType: string;
+  splitPayment: boolean = false;
 
   // currency mask
-
   currencyInputMask = createMask({
     prefix: '$',
     alias: 'numeric',
@@ -21,9 +23,35 @@ export class BillingSetupComponent implements OnInit {
     digitsOptional: false,
     placeholder: '0',
   });
-  constructor() { }
+
+  matterBillingSetting: MatterBillingSettings;
+  billingSettings: MatterBillingSettings;
+
+  constructor(private matterBillingSettingsService: MatterBillingSettingsService) {}
 
   ngOnInit(): void {
+    this.initBillingSettingsForm();
+    this.loadBillingSettings();
   }
 
+  private initBillingSettingsForm(): void {
+    this.billingSettings = new MatterBillingSettings(this.matter.id);
+  }
+
+  private loadBillingSettings() : void {
+    this.matterBillingSettingsService
+      .getWithMatterId(this.matter.id)
+      .subscribe((matterBillingSetting) => {
+        if (matterBillingSetting) {
+
+          this.billingSettings.setAllValues(matterBillingSetting);
+        }
+      });
+  }
+
+  upsertBillingSettings() {
+    this.matterBillingSettingsService.create(this.billingSettings).subscribe(res => {
+      console.log("billing settings upsert", res);
+    });
+  }
 }
