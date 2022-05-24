be15b24c4958f23ecb5abdeeb8453efda4831290
Bridger Jones <bridgerjones15@gmail.com>
2022-05-24 11:36:27 -0500

    implementing automation for flat rate in the intake billing setup where it generates the invoice objects

--- epfirms-client/src/app/core/interfaces/Invoice.ts
+++ epfirms-client/src/app/core/interfaces/Invoice.ts
@@ -12,17 +12,27 @@ export class Invoice {
     is_approved: boolean;
     is_client_visible: boolean;
 
-    constructor(matterId : number, userId: number, firmId: number, total: number) {
+    constructor(matterId : number, userId: number, firmId: number, total: number, description: string) {
         this.user_id = userId;
         this.firm_id = firmId;
         this.matter_id = matterId;
         this.invoice_id = "";
-        this.due_date = "";
-        this.description = "";
+        this.due_date = new Date(Date.now() + (1000 * 60 * 60 * 24 * 30)).toISOString();
+        this.description = description;
         this.status = "";
         this.total = total;
         this.hosted_invoice_url = "";
         this.is_approved = false;
         this.is_client_visible = false;
     }
+
+    setDate(date: string) {
+        this.due_date = date;
+    }
+
+    applySentProperties() : void {
+        this.is_approved = true;
+        this.is_client_visible = true;
+    }
+
 }
--- epfirms-client/src/app/features/intake-v2/billing-setup/billing-setup.component.ts
+++ epfirms-client/src/app/features/intake-v2/billing-setup/billing-setup.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Invoice } from '@app/core/interfaces/Invoice';
 import { Matter } from '@app/core/interfaces/matter';
 import { MatterBillingSettings } from '@app/core/interfaces/MatterBillingSettings';
+import { InvoiceService } from '@app/features/billing-v2/services/invoice.service';
 import { MatterBillingSettingsService } from '@app/shared/_services/matter-billing-settings-service/matter-billing-settings.service';
 import { createMask } from '@ngneat/input-mask';
 
@@ -27,7 +29,10 @@ export class BillingSetupComponent implements OnInit {
   matterBillingSetting: MatterBillingSettings;
   billingSettings: MatterBillingSettings;
 
-  constructor(private matterBillingSettingsService: MatterBillingSettingsService) {}
+  constructor(
+    private matterBillingSettingsService: MatterBillingSettingsService,
+    private invoiceService: InvoiceService,
+  ) {}
 
   ngOnInit(): void {
     this.initBillingSettingsForm();
@@ -38,23 +43,58 @@ export class BillingSetupComponent implements OnInit {
     this.billingSettings = new MatterBillingSettings(this.matter.id);
   }
 
-  private loadBillingSettings() : void {
+  private loadBillingSettings(): void {
     this.matterBillingSettingsService
       .getWithMatterId(this.matter.id)
       .subscribe((matterBillingSetting) => {
         if (matterBillingSetting) {
-
           this.billingSettings.setAllValues(matterBillingSetting);
         }
       });
   }
 
   upsertBillingSettings() {
-    this.matterBillingSettingsService.create(this.billingSettings).subscribe(res => {
-      console.log("billing settings upsert", res);
+    this.matterBillingSettingsService.create(this.billingSettings).subscribe((res) => {
+      console.log('billing settings upsert', res);
       if (res) {
         this.billingSettings.id = res[0].id;
       }
     });
+  }
+
+  submit(): void {
+    // handle the flat rate invoice automation
+    if (this.billingSettings.billing_type === 'flatrate') {
+      // if their is a split, there will be two invoices generated
+      // the intial due date is always 30 days from today
+      // the final invoice due date is the date the user enters
+      if (this.billingSettings.split_flat_rate) {
+        const initialInvoice = new Invoice(
+          this.matter.id,
+          this.matter.client.id,
+          this.matter.firm_id,
+          this.billingSettings.initial_payment,
+          this.billingSettings.initial_invoice_message,
+        );
+
+        const finalInvoice = new Invoice(
+          this.matter.id,
+          this.matter.client.id,
+          this.matter.firm_id,
+          this.billingSettings.final_payment,
+          this.billingSettings.final_invoice_message,
+        );
+
+        this.invoiceService.upsert(initialInvoice).subscribe((intial) => {
+          console.log('initial invoice', intial);
+        });
+        this.invoiceService.upsert(finalInvoice).subscribe((final) => {
+          console.log('final invoice', final);
+        });
+      }
+      // if not split
+      else {
+      }
+    }
   }
 }
