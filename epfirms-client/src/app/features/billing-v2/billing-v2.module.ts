7f184cfe8c1e83e51fc0ca528a3cba2857205062
bridger <bridger@epfirms.com>
2022-06-20 09:46:57 -0500

    removing dead components

--- epfirms-client/src/app/features/billing-v2/billing-v2.module.ts
+++ epfirms-client/src/app/features/billing-v2/billing-v2.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FirmBillingMainComponent } from './firm/firm-billing-main/firm-billing-main.component';
-import { FlatRateViewComponent } from './firm/flat-rate-view/flat-rate-view.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EditableModule } from '@app/shared/editable/editable.module';
-import { HourlyRateViewComponent } from './firm/hourly-rate-view/hourly-rate-view.component';
 import { InputMaskModule } from '@ngneat/input-mask';
 import { ComboboxModule } from '@app/shared/combobox/combobox.module';
 import { BillingV2RoutingModule } from './billing-v2-routing.module';
@@ -19,8 +17,6 @@ import { BillingSetupHelpOverlayComponent } from './firm/billing-setup-help-over
 @NgModule({
   declarations: [
     FirmBillingMainComponent,
-    FlatRateViewComponent,
-    HourlyRateViewComponent,
     CreateInvoiceOverlayComponent,
     BillingSetupHelpOverlayComponent
   ],
