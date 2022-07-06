a3160d8f743d015de8a06a6053b2d5c14d81ff53
bridger <bridger@epfirms.com>
2022-07-06 11:18:00 -0500

    adding new Apointee model with better property management

---
+++ epfirms-api/src/models/AppointeeV2.ts
@@ -0,0 +1,57 @@
+
+module.exports = (sequelize, {
+    INTEGER,
+    ENUM,
+  }) => {
+    const tableName = 'appointeev2';
+
+    const AppointeeV2 = sequelize.define(
+      tableName, {
+        id: {
+          type: INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        // this is a reference to the User object that holds the appointee information
+        user_id: {
+          type: INTEGER,
+          allowNull: false,
+        },
+        // this is a reference to the appointer's user table info
+        appointer_id: {
+          type: INTEGER,
+          allowNull: false
+        },
+        // these are the basic emumerations used for the appointee type
+        // the likely will be added to in the future
+        type: {
+          type: ENUM,
+            values: [
+                'executor',
+                'trustee',
+                //financial power of attorney
+                'fpoa',
+                // medical power of attorney
+                'mpoa',
+                // guardian of estate
+                'goe',
+                // guardian of person
+                'gop',
+                //guardian of minor
+                'gom',
+            ],
+        },
+        // ranking of the appointee compared to the others listed
+        rank : {
+          type: INTEGER
+        }
+     }, {
+        tableName,
+        defaultScope: {
+          attributes: { exclude: ['createdAt', 'updatedAt'] }
+        },
+      },
+    );
+
+    return AppointeeV2;
+  };
