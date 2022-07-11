0c419832e17c654273acadbfc3a44e164489f46c
bridger <bridger@epfirms.com>
2022-07-11 16:30:39 -0500

    fixing the appointeev2 model because I designed it wrong

--- epfirms-api/src/models/AppointeeV2.ts
+++ epfirms-api/src/models/AppointeeV2.ts
@@ -1,57 +1,86 @@
+module.exports = (sequelize, { INTEGER, BOOLEAN }) => {
+  const tableName = 'appointeev2';
 
-module.exports = (sequelize, {
-    INTEGER,
-    ENUM,
-  }) => {
-    const tableName = 'appointeev2';
-
-    const AppointeeV2 = sequelize.define(
-      tableName, {
-        id: {
-          type: INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-        },
-        // this is a reference to the User object that holds the appointee information
-        non_user_profile_id: {
-          type: INTEGER,
-          allowNull: false,
-        },
-        // this is a reference to the appointer's user table info
-        appointer_id: {
-          type: INTEGER,
-          allowNull: false
-        },
-        // these are the basic emumerations used for the appointee type
-        // the likely will be added to in the future
-        type: {
-          type: ENUM,
-            values: [
-                'executor',
-                'trustee',
-                //financial power of attorney
-                'fpoa',
-                // medical power of attorney
-                'mpoa',
-                // guardian of estate
-                'goe',
-                // guardian of person
-                'gop',
-                //guardian of minor
-                'gom',
-            ],
-        },
-        // ranking of the appointee compared to the others listed
-        rank : {
-          type: INTEGER
-        }
-     }, {
-        tableName,
-        defaultScope: {
-          attributes: { exclude: ['createdAt', 'updatedAt'] }
-        },
+  const AppointeeV2 = sequelize.define(
+    tableName,
+    {
+      id: {
+        type: INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
       },
-    );
+      // this is a reference to the User object that holds the appointee information
+      non_user_profile_id: {
+        type: INTEGER,
+        allowNull: false,
+      },
+      // this is a reference to the appointer's user table info
+      appointer_id: {
+        type: INTEGER,
+        allowNull: false,
+      },
 
-    return AppointeeV2;
-  };
+      // booleans that determine the type of appointee
+      // an appointee can be multiple types
+      is_executor: {
+        type: BOOLEAN,
+        defaultValue: false,
+      },
+
+      is_trustee: {
+        type: BOOLEAN,
+        defaultValue: false,
+      },
+      is_fpoa: {
+        type: BOOLEAN,
+        defaultValue: false,
+      },
+      is_mpoa: {
+        type: BOOLEAN,
+        defaultValue: false,
+      },
+      is_gom: {
+        type: BOOLEAN,
+        defaultValue: false,
+      },
+      is_gop: {
+        type: BOOLEAN,
+        defaultValue: false,
+      },
+      is_goe: {
+        type: BOOLEAN,
+        defaultValue: false,
+      },
+      // rank determines the priority in the appointment when compared to other appointees.
+      executor_rank: {
+        type: INTEGER,
+      },
+      trustee_rank: {
+        type: INTEGER,
+      },
+      fpoa_rank: {
+        type: INTEGER,
+      },
+      mpoa_rank: {
+        type: INTEGER,
+      },
+      gop_rank: {
+        type: INTEGER,
+      },
+      gom_rank: {
+        type: INTEGER,
+      },
+      goe_rank: {
+        type: INTEGER,
+      },
+    },
+    {
+      tableName,
+      defaultScope: {
+        attributes: { exclude: ['createdAt', 'updatedAt'] },
+      },
+    },
+  );
+
+  return AppointeeV2;
+};
